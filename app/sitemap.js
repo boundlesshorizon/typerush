e5d0cc27fcbd1b09a2e5d931049a209d997a5b83
Boundless Horizon <christian@boundlesshorizon.com>
2026-06-11 14:07:08 +0800

    Fix sitemap URL

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,7 +1,7 @@
 export default function sitemap() {
   return [
     {
-      url: 'https://typerush.app',
+      url: 'https://typerush-blond.vercel.app',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1,
